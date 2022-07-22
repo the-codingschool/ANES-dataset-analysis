0d7b9aa3ca5cb0ea8060c3f9a8b4570500257088
Wei Dewdney <realzhengwei06@gmail.com>
2022-07-22 13:30:11 -0400

    there is no salvation

--- WeisProject/weiScripts/initialExploration.R
+++
@@ -1,8 +0,0 @@
-#downloading libraries
-library(ggplot2)
-library(dplyr)
-
-#loading in csv file + saving as r object
-anes <- read.csv("WeisProject/anes_timeseries_2020_wei.csv")
-saveRDS(anes, "WeisProject/anes_timeseries_2020_wei.csv")
-View(anes)
