d17b220e619618bee3e2ccebe6bf0541f0cfcf9a
Y Liu <yl12@gmx.com>
2022-07-29 13:28:04 -0400

    working

--- YL-project/working.R
+++ YL-project/working.R
@@ -19,13 +19,45 @@ cor(votes_pres$V201033,votes_pres$V201042)
 iris_len <- nrow(votes_pres)
 votes_pres$label <- c(rep("train", ceiling(.9*iris_len)), rep("test", ceiling(.1*iris_len)))
 
-lmmodel <- gbm(V201033 ~ V201115 + V201121 + V201122 + V201124 + V201130 + V201133 + V201142 + V201136 + V201239 + V201240 + V201252 + V201255, data=votes_pres %>% filter(label=="test"),n.trees=500,distribution="gaussian")
+lmmodel <- gbm(V201033 ~ V201115 + V201121 + V201122 + V201124 + V201130 + V201133 + V201142 + V201136 + V201239 + V201240 + V201252 + V201255, data=votes_pres %>% filter(label=="train"),n.trees=500,distribution="gaussian")
 summary(lmmodel)
+
+lmmodel2 <- lm(V201033 ~ V201115 + V201121 + V201122 + V201124 + V201130 + V201133 + V201142 + V201136 + V201239 + V201240 + V201252 + V201255, data=votes_pres %>% filter(label=="train"))
+summary(lmmodel2)
+
 
 votes_pred <- votes_pres %>% filter(label=="test") %>% predict(object=lmmodel)
 dd <- votes_pres %>% filter(label=="test") %>% select(V201033, V201121)
-dd$pred <- votes_pred
-view(dd)
+dd$pred <- round(votes_pred)
+dd$pred2 <- votes_pres %>% filter(label=="test") %>% predict(object=lmmodel2)
+dd$pred2 <- round(dd$pred2)
+View(dd)
+
+library(Metrics)
+
+mae(dd$V201033, dd$pred)
+mae(dd$V201033, dd$pred2)
+
+try_with_n_trees <- function(trees) {
+  lmmodel <- gbm(V201033 ~ V201115 + V201121 + V201122 + V201124 + V201130 + V201133 + V201142 + V201136 + V201239 + V201240 + V201252 + V201255, data=votes_pres %>% filter(label=="train"),n.trees=trees,distribution="gaussian")
+  votes_pred <- votes_pres %>% filter(label=="test") %>% predict(object=lmmodel)
+  dd <- votes_pres %>% filter(label=="test") %>% select(V201033, V201121)
+  dd$pred <- round(votes_pred)
+  return(mae(dd$V201033, dd$pred))
+}
+try_with_n_trees(1)
+
+perf <- data.frame(
+  c(.04242424, .04242424, .04242424,.03636364,.03030303,.02626263),
+  c(2,3,4,15,20,25)
+)
+colnames(perf) <- c("Performance", "Trees")
+
+
+ggplot(data=perf, aes(x=Trees, y=Performance)) +
+  geom_line()
+
+
 
 
 votes <- data$V201033
