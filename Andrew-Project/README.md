98075d19957333f063728e85d1319673d6f09e95
Andrew Qian <andrew.qian88@gmail.com>
2022-07-29 13:03:33 -0700

    Commits

--- Andrew-Project/README.md
+++ Andrew-Project/README.md
@@ -1,4 +1,7 @@
 This is Andrew's project on the ANES Dataset for the Coding School Summer Research Program 2022.
 
 Here are possible research question ideas.
-Does one's political party affiliation affect their trust in the media? If so, which political party's supporters are more likely to be skeptical of the media?
+
+Does one's political party affiliation affect their belief of media bias? If so, which political party's supporters are more likely to be skeptical of the media?
+
+Which political party is more likely to believe in media bias?
--- Code #1.R
+++ Code #1.R
@@ -6,7 +6,7 @@ library(ggplot2)
 df <- read_csv("anes_timeseries_2020_csv_20220210.csv")
 table(df['V201021']) #
 table(df['V201376']) #How concerned are you that the government might undermine the media?
-table(df['V201376']) #How much do you trust the media?
+table(df['V201377']) #How much do you trust the media?
 table(df['V202064'])
 table(df['V202429']) #which party represents your views the best?
 
@@ -44,5 +44,57 @@ data$Q1 <- sub(0, "yes", data$Q1)
 
 data$Q1 <- recode(data$Q1, "0" = "Yes", "1" = "No")
 
+fdf <- filter(df, 'V202429' == 1)
+fdf
+none <- filter(fdf, 'V201376' == 1)
+
+none
+df
+
+filter(V202429, 'V202429' == 1)
+View(iris)
 
 
+df(c(1:8281), c(181))
+
+
+filtered <- filter(df, V202429 == 1 & V201377 == 5) #285
+
+filter(df, V202429 == 1 & V201377 == 1) #110
+
+
+
+df %>%
+  filter(V201377 == 3) %>%
+  ggplot(df, mapping=aes(x = as.factor(V202429), y = V201377)) +
+  geom_bar(stat='identity', color = "light blue", fill = "light blue")+
+  theme(axis.text.x = element_text(angle = 45, vjust = 0.7, hjust = 0.75)) +
+  scale_x_discrete(breaks = c(-9,-7,-6,-5,-1,1,5,7),
+                   labels = c("Refused", "No post-election data", "No post-election interview",
+                              "Interview Breakoff", "Inapplicable",
+                              "Democratic", "Republican", "Other")) +
+  labs(x = "Categories",
+       y = "Count",
+       title = "Count of Respondents Having a Great Deal of Trust in the Media") 
+  
+
+
+df %>%
+  filter(df, V202429 == 1 & V201377 == 3) #& V203401 == 1
+  ggplot(df, aes(x = as.factor(V202429), y = V201377)) +
+  geom_bar(stat='identity', color = "light blue", fill = "light blue")+
+  theme(axis.text.x = element_text(angle = 45, vjust = 0.7, hjust = 0.75)) +
+  scale_x_discrete(breaks = c(-9,-7,-6,-5,-1,1,5,7),
+                   labels = c("Refused", "No post-election data", "No post-election interview",
+                                "Interview Breakoff", "Inapplicable",
+                                "Democratic", "Republican", "Other")) +
+  labs(x = "Categories",
+         y = "Count",
+         title = "Count of Respondents Having a Great Deal of Trust in the Media") 
+  
+  
+  
+  scale_y_discrete(breaks = c(-9,-8,1,2,3,4,5),
+                   labels = c("Refused", "Don't Know", "None",
+                              "A little", "A moderate amount",
+                              "A lot", "A great deal")) +
